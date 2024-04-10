83a47a37d9ec79941d9b6ee84dfa8d04cb5382b1
Vite <omar.vite.a@uni.pe>
2024-04-09 21:13:44 -0500

    feat: uploaded testing activity environment

---
+++ Actividad--JUnit5_Testing/Código/src/test/java/org/example/MoneyTest.java
@@ -0,0 +1,11 @@
+package org.example;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MoneyTest {
+    @Test
+    void constructorShouldSetAmountAndCurrency() {
+        // Completa
+    }
+
+}
